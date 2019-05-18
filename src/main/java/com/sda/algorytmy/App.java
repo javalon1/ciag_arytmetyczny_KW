2016a0dbff26181a56496427f7990cb0851a2765
Karolina Wanat <carolinawanat@gmail.com>
2019-05-18 11:47:58 +0100

    arithmetic series

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,21 @@
 package com.sda.algorytmy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("wpisz liczbe wyrazow do wygenerowania");
+        int n = scanner.nextInt();
+        System.out.println("wpisz pierwszy wyraz ciagu");
+        int a = scanner.nextInt();
+        System.out.println("wpisz przyrost");
+        int d = scanner.nextInt();
+
+        for (int i = 0; i <= n ; i++) {
+            System.out.println(a + (i-1) *d);
+        }
+
     }
 }
